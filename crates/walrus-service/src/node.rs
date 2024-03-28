d3323076e10a030ebbc56503102b72f22aec6d46
Alberto Sonnino <alberto@sonnino.com>
2024-03-28 16:06:37 +0200

    Format doc string

--- crates/walrus-service/src/node.rs
+++ crates/walrus-service/src/node.rs
@@ -120,9 +120,14 @@ pub trait ServiceState {
     ) -> impl Future<Output = Result<Option<StorageConfirmation>, anyhow::Error>> + Send;
 
     /// Retrieves a recovery symbol for a shard held by this storage node.
-    /// The sliver_type is the target type of the sliver that will be recovered
-    /// The sliver_pair_idx is the index of the sliver pair that we want to access
-    /// The `index` is the pair index of the sliver to be recovered.
+    ///
+    /// # Arguments:
+    ///
+    /// * `sliver_type` - The target type of the sliver that will be recovered
+    /// * `sliver_pair_idx` - The index of the sliver pair that we want to access
+    /// * `index` - The pair index of the sliver to be recovered.
+    ///
+    /// Returns the recovery symbol for the requested sliver.
     fn retrieve_recovery_symbol(
         &self,
         blob_id: &BlobId,
