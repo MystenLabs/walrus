a2fbc987e746f3d3160caed132480d54229b6027
Jean-Pierre Smith <jean-pierre@mystenlabs.com>
2024-05-03 20:08:36 +0200

    refactor: use update_status instead of Ord for updating BlobCertificationStatus

--- crates/walrus-service/src/storage/blob_info.rs
+++ crates/walrus-service/src/storage/blob_info.rs
@@ -17,38 +17,24 @@ pub(crate) trait Mergeable {
     fn merge(self, operand: Self::MergeOperand) -> Self;
 }
 
-#[derive(Debug, Deserialize, Serialize, PartialEq, Eq, Clone, Copy)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Clone, Copy)]
 #[repr(u8)]
 pub enum BlobCertificationStatus {
-    Registered = 0,
-    Certified = 1,
-    Invalid = 2,
+    // INV: The indices of these must not change after release.
+    // BCS uses the index in encoding enums. Changing the index will change
+    // the database representation.
+    Registered,
+    Certified,
+    Invalid,
 }
 
-impl PartialOrd for BlobCertificationStatus {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
+impl BlobCertificationStatus {
+    pub(crate) fn update_status(self, new_status: Self) -> Self {
+        use BlobCertificationStatus::*;
 
-// Explicitly implement `Ord` to prevent issues from extending or otherwise
-// changing `BlobCertificationStatus` in the future.
-impl Ord for BlobCertificationStatus {
-    fn cmp(&self, other: &Self) -> Ordering {
-        match self {
-            BlobCertificationStatus::Invalid => match other {
-                BlobCertificationStatus::Invalid => Ordering::Equal,
-                _ => Ordering::Greater,
-            },
-            BlobCertificationStatus::Certified => match other {
-                BlobCertificationStatus::Invalid => Ordering::Less,
-                BlobCertificationStatus::Certified => Ordering::Equal,
-                BlobCertificationStatus::Registered => Ordering::Greater,
-            },
-            BlobCertificationStatus::Registered => match other {
-                BlobCertificationStatus::Registered => Ordering::Equal,
-                _ => Ordering::Less,
-            },
+        match (self, new_status) {
+            (Registered, Certified) | (Registered, Invalid) | (Certified, Invalid) => new_status,
+            _ => self,
         }
     }
 }
@@ -139,7 +125,7 @@ impl Mergeable for BlobInfo {
         match operand {
             BlobInfoMergeOperand::ChangeStatus { end_epoch, status } => {
                 self.end_epoch = self.end_epoch.max(end_epoch);
-                self.status = self.status.max(status);
+                self.status = self.status.update_status(status);
             }
             BlobInfoMergeOperand::MarkMetadataStored => {
                 self.is_metadata_stored = true;
@@ -201,6 +187,42 @@ impl From<&BlobEvent> for BlobInfoMergeOperand {
             BlobEvent::Registered(event) => event.into(),
             BlobEvent::Certified(event) => event.into(),
             BlobEvent::InvalidBlobID(event) => event.into(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use walrus_test_utils::{assert_unordered_eq, param_test};
+    use BlobCertificationStatus::*;
+
+    use super::*;
+
+    param_test! {
+        certification_status_transitions_to_new_status: [
+            registered: (Registered, &[Certified, Invalid]),
+            certified: (Certified, &[Invalid]),
+            invalid: (Invalid, &[]),
+        ]
+    }
+    fn certification_status_transitions_to_new_status(
+        original: BlobCertificationStatus,
+        must_transition_to: &[BlobCertificationStatus],
+    ) {
+        const ALL_STATUSES: &[BlobCertificationStatus] = &[Registered, Certified, Invalid];
+
+        let must_cases = must_transition_to.iter().map(|status| (status, status));
+        let never_cases = ALL_STATUSES
+            .iter()
+            .filter(|status| !must_transition_to.contains(status))
+            .map(|status| (status, &original));
+
+        for (&status, &expected) in must_cases.chain(never_cases) {
+            let actual = original.update_status(status);
+            assert_eq!(
+                actual, expected,
+                "'{original:?}' when updated with '{status:?}' should result in '{expected:?}': actual='{actual:?}'"
+            );
         }
     }
 }
