2bc4e569d4342f729e7dd73fdc8c541886804ffb
Markus Legner <markus@mystenlabs.com>
2024-05-24 11:14:09 +0200

    feat(client): consolidate client and aggregator in one binary

--- scripts/local-testbed.sh
+++ scripts/local-testbed.sh
@@ -133,6 +133,13 @@ You can then read the stored file by running the following (replacing "\$BLOB_ID
 returned by the store operation):
     cargo run --bin walrus-client -- --config working_dir/client_config.yaml read \$BLOB_ID
 
+You can also run a local aggregator service as follows:
+    ADDRESS="127.0.0.1:12345"
+    cargo run --bin walrus-client -- \\
+        --config working_dir/client_config.yaml aggregator --bind-address \$ADDRESS
+This allows you to read blobs through HTTP GET requests. For example, with curl:
+    curl http://\$ADDRESS/v1/\$BLOB_ID
+
 For further insights and debugging, you can increase the logging level for the Walrus code:
     export RUST_LOG="walrus=DEBUG"
 EOF
