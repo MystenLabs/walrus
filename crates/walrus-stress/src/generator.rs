0db5fa7c1ee8b81b090d29f7d10e456e43b723c6
Alberto Sonnino <alberto@sonnino.com>
2024-05-29 14:08:55 +0100

    feat: load generator (first draft)

---
+++ crates/walrus-stress/src/generator.rs
@@ -0,0 +1,225 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+//! Generate transactions for the stress test.
+
+use std::{sync::Arc, time::Duration};
+
+use rand::{rngs::StdRng, Rng, SeedableRng};
+use tokio::{
+    sync::{
+        mpsc::{channel, Receiver, Sender},
+        Notify,
+    },
+    task::JoinHandle,
+    time::sleep,
+};
+use walrus_core::{encoding::SliverPair, metadata::VerifiedBlobMetadataWithId};
+use walrus_service::{
+    cli_utils::load_wallet_context,
+    client::{Client, Config},
+};
+use walrus_sui::client::SuiContractClient;
+
+use crate::StressParameters;
+
+pub fn create_random_blob(rng: &mut StdRng, blob_size: usize) -> Vec<u8> {
+    (0..blob_size).map(|_| rng.gen()).collect()
+}
+
+/// Create a simple Walrus client. This client does not interacts with the chain but simply reads
+/// and writes blobs. The walrus nodes are expected to be running without checking whether the
+/// metadata associated with the blobs are stored on the chain.
+pub async fn create_walrus_client(
+    config: Config,
+    stress_parameters: &StressParameters,
+) -> anyhow::Result<Client<SuiContractClient>> {
+    let wallet_path = config.wallet_config.clone();
+    let wallet = load_wallet_context(&wallet_path)?;
+    let sui_client = SuiContractClient::new(
+        wallet,
+        config.system_pkg,
+        config.system_object,
+        stress_parameters.gas_budget,
+    )
+    .await?;
+    let client = Client::new(config, sui_client).await?;
+    Ok(client)
+}
+
+pub async fn reserve_blob(
+    config: Config,
+    stress_parameters: &StressParameters,
+    rng: &mut StdRng,
+) -> anyhow::Result<(
+    Client<SuiContractClient>,
+    Vec<SliverPair>,
+    VerifiedBlobMetadataWithId,
+)> {
+    let client = create_walrus_client(config, stress_parameters).await?;
+    let blob_size = stress_parameters.blob_size;
+    let blob = create_random_blob(rng, blob_size);
+    let (pairs, metadata) = client
+        .encoding_config()
+        .get_blob_encoder(&blob)?
+        .encode_with_metadata();
+    let epochs_ahead = 2;
+    let _blob_sui_object = client
+        .reserve_blob(&metadata, blob.len(), epochs_ahead)
+        .await?;
+
+    // TODO: Detect when the blob is stored by querying the storage nodes.
+    sleep(Duration::from_secs(1)).await;
+
+    Ok((client, pairs, metadata))
+}
+
+/// Make dumb (but valid) write transactions.
+#[derive(Debug)]
+pub struct WriteTransactionGenerator {
+    notify: Arc<Notify>,
+    _tx_transaction: Sender<(
+        Client<SuiContractClient>,
+        Vec<SliverPair>,
+        VerifiedBlobMetadataWithId,
+    )>,
+    rx_transaction: Receiver<(
+        Client<SuiContractClient>,
+        Vec<SliverPair>,
+        VerifiedBlobMetadataWithId,
+    )>,
+    _handler: JoinHandle<anyhow::Result<()>>,
+}
+
+impl WriteTransactionGenerator {
+    /// Start the write transaction generator.
+    pub async fn start(
+        config: Config,
+        stress_parameters: StressParameters,
+        pre_generation: usize,
+    ) -> anyhow::Result<Self> {
+        let (tx_transaction, rx_transaction) = channel(pre_generation * 10 + 1);
+        let notify = Arc::new(Notify::new());
+        let cloned_notify = notify.clone();
+
+        let mut rng = StdRng::from_entropy();
+
+        // Generate new transactions in the background.
+        let sender = tx_transaction.clone();
+        let handler = tokio::spawn(async move {
+            let mut i = 0;
+            loop {
+                if i % pre_generation == 0 {
+                    tracing::debug!("Generated {i} tx");
+                }
+
+                // Generate a new transaction.
+                let (client, pairs, metadata) =
+                    reserve_blob(config.clone(), &stress_parameters, &mut rng).await?;
+
+                i += 1;
+                if i == pre_generation {
+                    cloned_notify.notify_one();
+                }
+
+                // This call blocks when the channel is full.
+                sender
+                    .send((client, pairs, metadata))
+                    .await
+                    .expect("Failed to send tx");
+                tokio::task::yield_now().await;
+            }
+        });
+
+        Ok(Self {
+            notify,
+            _tx_transaction: tx_transaction,
+            rx_transaction,
+            _handler: handler,
+        })
+    }
+
+    /// Get a new transaction.
+    pub async fn make_tx(
+        &mut self,
+    ) -> (
+        Client<SuiContractClient>,
+        Vec<SliverPair>,
+        VerifiedBlobMetadataWithId,
+    ) {
+        self.rx_transaction
+            .recv()
+            .await
+            .expect("Failed to get new write tx")
+    }
+
+    /// Wait for the generator to finish pre-generating transactions.
+    pub async fn initialize(&self) {
+        self.notify.notified().await;
+    }
+}
+
+// Make dumb (but valid) read transactions.
+#[derive(Debug)]
+pub struct ReadTransactionGenerator {
+    notify: Arc<Notify>,
+    _tx_transaction: Sender<Client<SuiContractClient>>,
+    rx_transaction: Receiver<Client<SuiContractClient>>,
+    _handler: JoinHandle<anyhow::Result<()>>,
+}
+
+impl ReadTransactionGenerator {
+    /// Start the write transaction generator.
+    pub async fn start(
+        config: Config,
+        stress_parameters: StressParameters,
+        pre_generation: usize,
+    ) -> anyhow::Result<Self> {
+        let (tx_transaction, rx_transaction) = channel(pre_generation * 10 + 1);
+        let notify = Arc::new(Notify::new());
+        let cloned_notify = notify.clone();
+
+        // Generate new transactions in the background.
+        let sender = tx_transaction.clone();
+        let handler = tokio::spawn(async move {
+            let mut i = 0;
+            loop {
+                if i % pre_generation == 0 {
+                    tracing::debug!("Generated {i} tx");
+                }
+
+                // Generate a new transaction.
+                let client = create_walrus_client(config.clone(), &stress_parameters).await?;
+
+                i += 1;
+                if i == pre_generation {
+                    cloned_notify.notify_one();
+                }
+
+                // This call blocks when the channel is full.
+                sender.send(client).await.expect("Failed to send tx");
+                tokio::task::yield_now().await;
+            }
+        });
+
+        Ok(Self {
+            notify,
+            _tx_transaction: tx_transaction,
+            rx_transaction,
+            _handler: handler,
+        })
+    }
+
+    /// Get a new transaction.
+    pub async fn make_tx(&mut self) -> Client<SuiContractClient> {
+        self.rx_transaction
+            .recv()
+            .await
+            .expect("Failed to get new write tx")
+    }
+
+    /// Wait for the generator to finish pre-generating transactions.
+    pub async fn initialize(&self) {
+        self.notify.notified().await;
+    }
+}
---
+++ crates/walrus-stress/src/main.rs
@@ -0,0 +1,222 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+//! Load generators for stress testing the Walrus nodes.
+
+use std::{
+    net::{IpAddr, Ipv4Addr, SocketAddr},
+    path::PathBuf,
+    time::Duration,
+};
+
+use anyhow::Context;
+use clap::Parser;
+use futures::{stream::FuturesUnordered, StreamExt};
+use generator::WriteTransactionGenerator;
+use rand::{rngs::StdRng, SeedableRng};
+use serde::{Deserialize, Serialize};
+use tokio::time::{interval, Instant};
+use walrus_core::{encoding::Primary, BlobId};
+use walrus_service::{client::Config, config::LoadConfig};
+
+use crate::{generator::ReadTransactionGenerator, metrics::ClientMetrics};
+
+mod generator;
+mod metrics;
+
+/// Timing burst precision.
+const PRECISION: u64 = 10;
+/// Duration of each burst of transaction.
+const BURST_DURATION: Duration = Duration::from_millis(1000 / PRECISION);
+
+/// The parameters for the stress test.
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct StressParameters {
+    /// The gas budget for transactions.
+    pub gas_budget: u64,
+    /// The percentage of write requests. If this is 0, only read requests are made;
+    /// if this is 100, only write requests are made; otherwise, a mix of read and write
+    /// requests are made.
+    pub load_type: u64,
+    /// The size of the blob to read and write.
+    pub blob_size: usize,
+    /// The address to expose the metrics.
+    pub metrics_port: u16,
+}
+
+impl LoadConfig for StressParameters {}
+
+#[derive(Parser, Debug, Clone)]
+#[clap(rename_all = "kebab-case")]
+#[command(author, version, about = "Walrus load generator", long_about = None)]
+struct Args {
+    /// The load to submit to the system (tx/s).
+    #[clap(long)]
+    load: u64,
+    /// The path to the wallet configuration file.
+    #[clap(long)]
+    config_path: PathBuf,
+    /// Path to the load parameters file.
+    #[clap(long)]
+    stress_parameters_path: PathBuf,
+    /// The duration of the benchmark in seconds (used to estimate the number of
+    /// transactions to pre-generate)
+    #[clap(long, value_parser = parse_duration)]
+    duration: Duration,
+}
+
+fn parse_duration(arg: &str) -> Result<Duration, std::num::ParseIntError> {
+    let seconds = arg.parse()?;
+    Ok(Duration::from_secs(seconds))
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    let config = Config::load(args.config_path).context("Failed to load client config")?;
+    let stress_parameters = StressParameters::load(&args.stress_parameters_path)
+        .context("Failed to load stress parameters")?;
+    let percentage_writes = stress_parameters.load_type.min(100);
+    let duration = args.duration.as_secs();
+
+    // Start the metrics server.
+    let metrics_address = SocketAddr::new(
+        IpAddr::V4(Ipv4Addr::UNSPECIFIED),
+        stress_parameters.metrics_port,
+    );
+    let registry_service = mysten_metrics::start_prometheus_server(metrics_address);
+    let prometheus_registry = registry_service.default_registry();
+    let metrics = ClientMetrics::new(&prometheus_registry);
+
+    // Start the write transaction generator.
+    tracing::info!("Initializing write transactions generators...");
+    let write_pre_compute = (args.load * duration * percentage_writes) / 100;
+    let write_tx_generator = WriteTransactionGenerator::start(
+        config.clone(),
+        stress_parameters.clone(),
+        write_pre_compute as usize,
+    )
+    .await
+    .context("Failed to start write transaction generator")?;
+    write_tx_generator.initialize().await;
+
+    // Make one write transaction (which will be used as a template for the read transactions).
+    tracing::info!("Submitting one write transaction...");
+    let mut rng = StdRng::from_entropy();
+    let blob_size = stress_parameters.blob_size;
+    let blob = generator::create_random_blob(&mut rng, blob_size);
+    let epochs_ahead = 2;
+    let client = generator::create_walrus_client(config.clone(), &stress_parameters)
+        .await
+        .context("Failed to create Walrus client")?;
+    let blob = client
+        .reserve_and_store_blob(&blob, epochs_ahead)
+        .await
+        .context("Failed to reserve and store blob")?;
+
+    // Start the read transaction generator.
+    tracing::info!("Initializing read transactions generators...");
+    let read_pre_compute = (args.load * duration * (100 - percentage_writes)) / 100;
+    let read_tx_generator = ReadTransactionGenerator::start(
+        config,
+        stress_parameters.clone(),
+        read_pre_compute as usize,
+    )
+    .await
+    .context("Failed to start read transaction generator")?;
+    read_tx_generator.initialize().await;
+
+    // Start the benchmark.
+    tracing::info!("Start sending transactions");
+    benchmark(
+        args.load,
+        &stress_parameters,
+        write_tx_generator,
+        read_tx_generator,
+        &blob.blob_id,
+        &metrics,
+    )
+    .await
+    .context("Failed to run benchmark")?;
+
+    Ok(())
+}
+
+async fn benchmark(
+    load: u64,
+    stress_parameters: &StressParameters,
+    mut write_tx_generator: WriteTransactionGenerator,
+    mut read_tx_generator: ReadTransactionGenerator,
+    blob_id: &BlobId,
+    metrics: &ClientMetrics,
+) -> anyhow::Result<()> {
+    let burst = load / PRECISION;
+    let writes_per_burst = (burst * stress_parameters.load_type) / 100;
+    let reads_per_burst = burst - writes_per_burst;
+
+    // Structures holding futures waiting for clients to finish their requests.
+    let mut write_finished = FuturesUnordered::new();
+    let mut read_finished = FuturesUnordered::new();
+
+    // Submit transactions.
+    let start = Instant::now();
+    let interval = interval(BURST_DURATION);
+    tokio::pin!(interval);
+
+    loop {
+        tokio::select! {
+            _ = interval.tick() => {
+                // Generate the transactions for this burst.
+                let mut write_load = Vec::new();
+                for _ in 1..=writes_per_burst {
+                    write_load.push(write_tx_generator.make_tx().await);
+                }
+                let mut read_load = Vec::new();
+                for _ in 1..=reads_per_burst {
+                    read_load.push(read_tx_generator.make_tx().await);
+                }
+
+                // Submit those transactions.
+                let now = Instant::now();
+                let duration_since_start = now.duration_since(start);
+                metrics.observe_benchmark_duration(duration_since_start);
+
+                for (client, pairs, metadata) in write_load {
+                    metrics.observe_submitted(metrics::WRITE_WORKLOAD);
+
+                    write_finished.push(async move {
+                        let certificate = client.store_metadata_and_pairs(&metadata, &pairs).await;
+                        (now, certificate)
+                    });
+                }
+                for client in read_load {
+                    metrics.observe_submitted(metrics::READ_WORKLOAD);
+
+                    read_finished.push(async move {
+                        let blob = client.read_blob::<Primary>(blob_id).await;
+                        (now, blob)
+                    });
+                }
+
+                // Check if the submission rate is too high.
+                if now.elapsed() > BURST_DURATION {
+                    metrics.observe_error("rate too high");
+                    tracing::warn!("Transaction rate too high for this client");
+                }
+            },
+            Some((instant, result)) = write_finished.next() => {
+                let _certificate = result.context("Failed to obtain storage certificate")?;
+                let elapsed = instant.elapsed();
+                metrics.observe_latency(metrics::WRITE_WORKLOAD, elapsed);
+            },
+            Some((instant, result)) = read_finished.next() => {
+                let _blob = result.context("Failed to obtain blob")?;
+                let elapsed = instant.elapsed();
+                metrics.observe_latency(metrics::READ_WORKLOAD, elapsed);
+            },
+            else => break
+        }
+    }
+
+    Ok(())
+}
