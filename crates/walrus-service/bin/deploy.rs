8a28a8e25b392529194e21b8371d856ee9faf42f
giac-mysten <giacomo@mystenlabs.com>
2024-08-12 17:20:24 +0100

    docs: add description
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>

--- crates/walrus-service/bin/deploy.rs
+++ crates/walrus-service/bin/deploy.rs
@@ -48,6 +48,9 @@ struct DeploySystemContractArgs {
     #[clap(long, default_value = "./working_dir")]
     working_dir: PathBuf,
     /// Sui network for which the config is generated.
+    ///
+    /// Available options are `devnet`, `testnet`, and `localnet`, or a custom Sui network. To
+    /// specify a custom Sui network, pass a string of the format `<RPC_URL>;<FAUCET_URL>`.
     #[clap(long, default_value = "testnet")]
     sui_network: SuiNetwork,
     /// The directory in which the contracts are located.
