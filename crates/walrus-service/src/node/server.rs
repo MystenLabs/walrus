7d24f0495e458033c40ebd1b3d3faa2e54deb1c8
giac-mysten <giacomo@mystenlabs.com>
2024-08-19 17:19:36 +0100

    feat(service): add endpoint for sliver status
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>

--- crates/walrus-service/src/node/server.rs
+++ crates/walrus-service/src/node/server.rs
@@ -84,7 +84,7 @@ where
                 routes::INCONSISTENCY_PROOF_ENDPOINT,
                 put(routes::inconsistency_proof),
             )
-            .route(routes::STATUS_ENDPOINT, get(routes::get_blob_status))
+            .route(routes::BLOB_STATUS_ENDPOINT, get(routes::get_blob_status))
             .route(routes::HEALTH_ENDPOINT, get(routes::health_info))
             .route(routes::SYNC_SHARD_ENDPOINT, post(routes::sync_shard))
             .with_state(self.state.clone())
@@ -157,7 +157,7 @@ mod test {
     use tokio::{task::JoinHandle, time::Duration};
     use tokio_util::sync::CancellationToken;
     use walrus_core::{
-        encoding::Primary,
+        encoding::{EncodingAxis, Primary},
         inconsistency::{
             InconsistencyProof as InconsistencyProofInner,
             InconsistencyVerificationError,
@@ -303,6 +303,18 @@ mod test {
                 Ok(BlobStatus::Nonexistent)
             } else {
                 Err(anyhow::anyhow!("Internal error").into())
+            }
+        }
+
+        fn is_sliver_stored<A: EncodingAxis>(
+            &self,
+            blob_id: &BlobId,
+            sliver_pair_index: SliverPairIndex,
+        ) -> Result<bool, RetrieveSliverError> {
+            if blob_id.0[0] == 0 && sliver_pair_index.as_usize() == 0 {
+                Ok(true)
+            } else {
+                Err(RetrieveSliverError::Unavailable)
             }
         }
 
