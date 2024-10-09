d798d369b77a2748030b3fe44d1f57b3910eba88
Jean-Pierre Smith <jean-pierre@mystenlabs.com>
2024-10-09 18:18:49 +0200

    feat: export logs as json, require defaults from the env

--- crates/walrus-service/src/common/utils.rs
+++ crates/walrus-service/src/common/utils.rs
@@ -449,7 +449,7 @@ impl MetricsAndLoggingRuntime {
         let (telemetry_guards, tracing_handle) = telemetry_subscribers::TelemetryConfig::new()
             .with_env()
             .with_prom_registry(&walrus_registry)
-            .with_log_level("debug")
+            .with_json()
             .init();
 
         Ok(Self {
