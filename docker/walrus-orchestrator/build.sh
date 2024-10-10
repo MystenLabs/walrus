177187aeafb33ac14dc99912cfdee2fd9a33f614
Pei Deng <pei.deng@mystenlabs.com>
2024-10-10 16:49:04 -0700

    ec lint

--- docker/walrus-orchestrator/build.sh
+++ docker/walrus-orchestrator/build.sh
@@ -5,7 +5,7 @@
 # fast fail.
 set -e
 
-DIR="$( cd "$( dirname "$0" )" && pwd )"
+DIR="$(cd "$(dirname "$0")" && pwd)"
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 DOCKERFILE="$DIR/Dockerfile"
 GIT_REVISION="$(git describe --always --abbrev=12 --dirty --exclude '*')"
@@ -13,11 +13,11 @@ BUILD_DATE="$(date -u +'%Y-%m-%d')"
 
 # option to build using debug symbols
 if [ "$1" = "--debug-symbols" ]; then
-	PROFILE="bench"
-	echo "Building with full debug info enabled ... WARNING: binary size might significantly increase"
-	shift
+  PROFILE="bench"
+  echo "Building with full debug info enabled ... WARNING: binary size might significantly increase"
+  shift
 else
-	PROFILE="release"
+  PROFILE="release"
 fi
 
 echo
@@ -29,7 +29,7 @@ echo "git revision: \t$GIT_REVISION"
 echo
 
 docker build -f "$DOCKERFILE" "$REPO_ROOT" \
-	--build-arg GIT_REVISION="$GIT_REVISION" \
-	--build-arg BUILD_DATE="$BUILD_DATE" \
-	--build-arg PROFILE="$PROFILE" \
-	"$@"
+  --build-arg GIT_REVISION="$GIT_REVISION" \
+  --build-arg BUILD_DATE="$BUILD_DATE" \
+  --build-arg PROFILE="$PROFILE" \
+  "$@"
