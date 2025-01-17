a22e38cb18d212fa1f3bc0bbb552a12bab65330b
Will Bradley <will.bradley@mystenlabs.com>
2025-01-17 14:12:34 -0700

    wip

--- crates/walrus-local-testbed/src/main.rs
+++ crates/walrus-local-testbed/src/main.rs
@@ -21,6 +21,7 @@ use tracing_subscriber::{
 #[derive(Parser, Debug)]
 #[clap(name = "walrus-runner")]
 struct Args {
+    /// Tail the logs of the spawned nodes.
     #[clap(short, long, default_value = "false")]
     tail_logs: bool,
 
@@ -38,6 +39,9 @@ struct Args {
 
     #[clap(short, long)]
     existing: bool,
+
+    #[clap(long)]
+    clean: bool,
 }
 
 fn kill_tmux_sessions() -> Result<()> {
@@ -66,30 +70,33 @@ fn kill_tmux_sessions() -> Result<()> {
 }
 
 fn run_node(working_dir: &Path, config_name: &str, cleanup_storage: bool) -> Result<()> {
-    Command::new("tmux")
-        .args([
-            "new",
-            "-d",
-            "-s",
+    let args: &[String] = &[
+        "new".into(),
+        "-d".into(),
+        "-s".into(),
+        config_name.into(),
+        shlex::try_quote(&format!(
+            "./target/release/walrus-node run --config-path {}/{}.yaml{}",
+            working_dir.display(),
             config_name,
-            &shlex::try_quote(&format!(
-                "./target/release/walrus-node run --config-path {}/{}.yaml{}",
-                working_dir.display(),
-                config_name,
-                if cleanup_storage {
-                    " --cleanup-storage"
-                } else {
-                    ""
-                }
-            ))?,
-        ])
+            if cleanup_storage {
+                " --cleanup-storage"
+            } else {
+                ""
+            }
+        ))?
+        .to_string(),
+    ];
+    tracing::info!("Starting node: {}", config_name);
+    let output = Command::new("tmux")
+        .args(args)
         .output()
         .context("Failed to start tmux session")?;
-
+    tracing::info!("tmux output: {}", String::from_utf8_lossy(&output.stdout));
     Ok(())
 }
 
-fn update_configs(working_dir: &Path) -> Result<()> {
+fn update_configs(working_dir: &Path, network: &str) -> Result<()> {
     // Read client config
     let client_config_path = working_dir.join("client_config.yaml");
     let client_config: Value = serde_yaml::from_reader(fs::File::open(&client_config_path)?)?;
@@ -103,7 +110,10 @@ fn update_configs(working_dir: &Path) -> Result<()> {
         (
             "sui".into(),
             serde_yaml::mapping::Mapping::from_iter([
-                ("rpc".into(), "https://fullnode.backup_dir".into()),
+                (
+                    "rpc".into(),
+                    format!("https://fullnode.{network}.sui.io:443").into(),
+                ),
                 (
                     "system_object".into(),
                     client_config["system_object"].clone(),
@@ -117,15 +127,22 @@ fn update_configs(working_dir: &Path) -> Result<()> {
         ),
     ]);
 
+    tracing::info!("Writing backup config: {:#?}", backup_config);
     // Write backup config
     serde_yaml::to_writer(
         fs::File::create(working_dir.join("backup_config.yaml"))?,
         &backup_config,
     )?;
 
-    // Update node configs
-    for entry in glob(&format!("{}/dryrun-node-*yaml", working_dir.display()))? {
+    // Update node configs to include adaptive downloader config.
+    for entry in glob(&format!("{}/dryrun-node-*.yaml", working_dir.display()))? {
         let path = entry?;
+        if path
+            .file_name()
+            .is_none_or(|filename| filename.to_str().is_none_or(|s| s.contains("-sui")))
+        {
+            continue;
+        }
         let mut config: Value = serde_yaml::from_reader(fs::File::open(&path)?)?;
 
         let event_processor_config = serde_yaml::mapping::Mapping::from_iter([(
@@ -239,12 +256,15 @@ fn main() -> Result<()> {
             ])
             .status()?;
 
-        update_configs(&working_dir)?;
+        update_configs(&working_dir, &args.network)?;
     }
 
     // Start nodes
     let mut node_count = 0;
-    for entry in glob(&format!("{}/dryrun-node-*yaml", working_dir.display()))? {
+    for entry in glob(&format!(
+        "{}/dryrun-node-[0-9]*.yaml",
+        working_dir.display()
+    ))? {
         let path = entry?;
         let node_name = path.file_stem().unwrap().to_str().unwrap();
         run_node(&working_dir, node_name, cleanup)?;
