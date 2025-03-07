5c428d5ca37ee078133f56ea7e19d48f808f285e
Antonio Yang <yanganto@gmail.com>
2025-03-07 14:16:26 +0800

    Update crates/walrus-service/src/client/daemon/auth.rs
    Co-authored-by: Markus Legner <mlegner@users.noreply.github.com>

--- crates/walrus-service/src/client/daemon/auth.rs
+++ crates/walrus-service/src/client/daemon/auth.rs
@@ -63,7 +63,7 @@ pub struct Claim {
 }
 
 impl Claim {
-    /// Build Claim from JWT token
+    /// Builds the Claim from a JWT token.
     pub fn from_token(
         token: &str,
         decoding_key: &DecodingKey,
