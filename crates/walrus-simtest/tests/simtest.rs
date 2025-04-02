acb4a57207f597bdb315a3b7e8b14253a66bff69
He Liu <he.liu@mystenlabs.com>
2025-04-02 11:30:05 -0700

    added check.

--- crates/walrus-simtest/tests/simtest.rs
+++ crates/walrus-simtest/tests/simtest.rs
@@ -312,6 +312,8 @@ mod tests {
             .get_latest_committees_in_test()
             .await
             .unwrap();
+        let current_committee = committees.current_committee();
+        assert!(current_committee.contains(&walrus_cluster.nodes[5].public_key));
 
         assert!(node_health_info[5].shard_detail.is_some());
 
