1c17bbd1f1d1ec786521fd7ca1c5273529c198ae
Will Bradley <will.bradley@mystenlabs.com>
2025-05-21 14:20:28 -0600

    wip

--- crates/fan-out-proxy/src/main.rs
+++ crates/fan-out-proxy/src/main.rs
@@ -1,18 +1,19 @@
 //! Walrus Fan-out Proxy entry point.
-use std::{env, net::SocketAddr, num::NonZeroU16};
+use std::{env, net::SocketAddr, num::NonZeroU16, path::PathBuf};
 
 use anyhow::Result;
 use axum::{
     Router,
     body::Bytes,
-    extract::Query,
+    extract::{Query, State},
     http::StatusCode,
-    response::{IntoResponse, Json, Response},
+    response::{IntoResponse, Json},
     routing::post,
 };
 use axum_server::bind;
 use clap::{Parser, Subcommand};
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
 use tokio::time::Instant;
 use tracing_subscriber::{EnvFilter, Layer, layer::SubscriberExt as _, util::SubscriberInitExt};
 use walrus_core::{
@@ -22,11 +23,16 @@ use walrus_core::{
     encoding::{DataTooLargeError, EncodingConfig, EncodingConfigTrait as _, SliverPair},
     metadata::BlobMetadataWithId,
 };
+use walrus_sdk::client::Client;
+use walrus_sui::client::{SuiContractClient, contract_config::ContractConfig};
+
+use crate::error::FanOutError;
+
+mod error;
 
 // TODO: Pull this from the active committee on chain.
 const N_SHARDS: NonZeroU16 = NonZeroU16::new(1000).expect("N_SHARDS must be non-zero");
 
-// This is an important refactoring.
 #[derive(Parser)]
 #[command(author, version, about)]
 struct Args {
@@ -37,8 +43,16 @@ struct Args {
 
 #[derive(Subcommand)]
 enum Command {
-    /// Runt the Walrus Fan-out Proxy.
-    Proxy,
+    /// Run the Walrus Fan-out Proxy.
+    Proxy {
+        /// The file path of the Fan-out Proxy operator's wallet. This is the account that will be
+        /// used to check for fan-out transactions that may have been received.
+        wallet_path: PathBuf,
+        /// Object ID of the Walrus system object.
+        system_object: ObjectID,
+        /// Object ID of the Walrus staking object.
+        staking_object: ObjectID,
+    },
 }
 
 fn init_logging() {
@@ -57,20 +71,46 @@ fn init_logging() {
         .init();
 }
 
+#[derive(Clone)]
+pub(crate) struct Controller {
+    client: Client<SuiContractClient>,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = Args::parse();
 
     init_logging();
-
     match args.command {
-        Command::Proxy => {
-            let app = Router::new().route("/v1/blob-fan-out", post(fan_out_blob_slivers));
+        Command::Proxy {
+            wallet_path,
+            system_object,
+            staking_object,
+        } => {
+            let client = get_client(wallet_path, system_object, staking_object).await?;
+            let app = Router::new()
+                .with_state(Arc::new(Controller { client }))
+                .route("/v1/blob-fan-out", post(fan_out_blob_slivers));
             let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
             tracing::info!(?addr, "Serving fan-out proxy");
             Ok(bind(addr).serve(app.into_make_service()).await?)
         }
     }
+}
+
+async fn get_client(
+    wallet_path: PathBuf,
+    system_object: ObjectID,
+    staking_object: ObjectID,
+) -> Result<Client<SuiContractClient>> {
+    let wallet =
+        load_wallet_context_from_path(wallet_path, None).context("unable to load wallet")?;
+    let contract_config = ContractConfig::new(system_object_id, staking_object_id);
+
+    #[allow(deprecated)]
+    let rpc_urls = &[wallet.get_rpc_url()?];
+
+    SuiContractClient::new(wallet, rpc_urls, &contract_config, Default::default(), None).await
 }
 
 #[derive(Debug, Deserialize)]
@@ -87,53 +127,15 @@ struct ResponseType {
     n_shards: u16,
 }
 
-use thiserror::Error;
-
-/// Fan-out Proxy Errors
-#[derive(Debug, Error)]
-pub enum Error {
-    /// Invalid input error.
-    #[error("Bad input: {0}")]
-    BadRequest(String),
-
-    /// Blob is too large error.
-    #[error(transparent)]
-    DataTooLargeError(#[from] DataTooLargeError),
-
-    /// Invalid BlobId error.
-    #[error(transparent)]
-    BlobIdParseError(#[from] BlobIdParseError),
-
-    /// Internal server error.
-    #[error(transparent)]
-    Other(#[from] anyhow::Error),
-}
-
-impl IntoResponse for Error {
-    fn into_response(self) -> Response {
-        match self {
-            Error::BadRequest(msg) => (StatusCode::BAD_REQUEST, msg).into_response(),
-            Error::DataTooLargeError(error) => {
-                (StatusCode::BAD_REQUEST, error.to_string()).into_response()
-            }
-            Error::BlobIdParseError(error) => {
-                (StatusCode::BAD_REQUEST, error.to_string()).into_response()
-            }
-            Error::Other(error) => {
-                (StatusCode::INTERNAL_SERVER_ERROR, error.to_string()).into_response()
-            }
-        }
-    }
-}
-
 async fn fan_out_blob_slivers(
+    State(controller): State<Arc<Controller>>,
     Query(params): Query<Params>,
     body: Bytes,
-) -> Result<impl IntoResponse, Error> {
+) -> Result<impl IntoResponse, FanOutError> {
     tracing::info!(?params, "fan_out_blob_slivers");
     // Validate "tx" length and hex-ness
     if params.tx.len() != 32 || !params.tx.chars().all(|c| c.is_ascii_hexdigit()) {
-        return Err(Error::BadRequest(format!(
+        return Err(FanOutError::BadRequest(format!(
             "Invalid tx parameter [tx={}]",
             params.tx
         )));
@@ -150,7 +152,7 @@ async fn fan_out_blob_slivers(
         .encode_with_metadata(blob)?;
     let duration = encode_start_timer.elapsed();
     if *metadata.blob_id() != blob_id {
-        return Err(Error::BadRequest(format!(
+        return Err(FanOutError::BadRequest(format!(
             "Blob ID mismatch [expected={}, actual={}]",
             blob_id,
             metadata.blob_id()
