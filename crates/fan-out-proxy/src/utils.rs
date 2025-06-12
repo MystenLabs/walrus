66635debf7556c42ec730f748ae6286d0469a705
giac-mysten <giacomo@mystenlabs.com>
2025-06-12 14:08:10 +0100

    feat(fanout): small tests
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>

--- crates/fan-out-proxy/src/utils.rs
+++ crates/fan-out-proxy/src/utils.rs
@@ -64,28 +64,21 @@ pub(crate) fn check_tx_auth_package(
     rcvd_nonce: &[u8; DIGEST_LEN],
     tx: SuiTransactionBlockResponse,
 ) -> Result<(), FanOutError> {
-    let orig_tx: SenderSignedData = bcs::from_bytes(&tx.raw_transaction)
-        .map_err(|_| FanOutError::other("error deserializing the transaction from bytes"))?;
+    let tx_auth_package = extract_hashed_auth_package(tx)?;
+    auth_package_checks(tx_auth_package, blob, rcvd_nonce)
+}
 
-    let TransactionData::V1(TransactionDataV1 {
-        kind: TransactionKind::ProgrammableTransaction(ptb),
-        ..
-    }) = orig_tx.transaction_data()
-    else {
-        return Err(FanOutError::other("invalid transaction data"));
-    };
-    let Some(CallArg::Pure(tx_auth_package_bytes)) = ptb.inputs.first() else {
-        return Err(FanOutError::other("invalid transaction input construction"));
-    };
-    let tx_auth_package: HashedAuthPackage = bcs::from_bytes(tx_auth_package_bytes)
-        .map_err(|_| FanOutError::other("cannot decode the bytes of input 0"))?;
-
-    // Authentication checks.
-    // NOTE: Ordering is important. First we discard attempts by unauthorized senders using the
-    // nonce, then we check the length, making sure that the blob-hashing work we will have to do is
-    // fixed (and has been paid for, through the tip check), and finally we check that the hash of
-    // the blob is matching.
-
+/// Performs the authentication checks on the hashed auth package in the transaction.
+///
+/// NOTE: Ordering is important. First we discard attempts by unauthorized senders using the
+/// nonce, then we check the length, making sure that the blob-hashing work we will have to do is
+/// fixed (and has been paid for, through the tip check), and finally we check that the hash of
+/// the blob is matching.
+pub(crate) fn auth_package_checks(
+    tx_auth_package: HashedAuthPackage,
+    blob: &[u8],
+    rcvd_nonce: &[u8; DIGEST_LEN],
+) -> Result<(), FanOutError> {
     // 1. Check that the nonce in the received package is the preimage of the one in the tx.
     let rcvd_nonce_digest = compute_digest_sha256(rcvd_nonce).digest;
     ensure!(
@@ -106,4 +99,94 @@ pub(crate) fn check_tx_auth_package(
         FanOutError::BlobDigestMismatch
     );
     Ok(())
+}
+
+/// Extracts the hashed auth package from the transaction.
+pub(crate) fn extract_hashed_auth_package(
+    tx: SuiTransactionBlockResponse,
+) -> Result<HashedAuthPackage, FanOutError> {
+    let orig_tx: SenderSignedData = bcs::from_bytes(&tx.raw_transaction)
+        .map_err(|_| FanOutError::other("error deserializing the transaction from bytes"))?;
+
+    let TransactionData::V1(TransactionDataV1 {
+        kind: TransactionKind::ProgrammableTransaction(ptb),
+        ..
+    }) = orig_tx.transaction_data()
+    else {
+        return Err(FanOutError::other("invalid transaction data"));
+    };
+    let Some(CallArg::Pure(tx_auth_package_bytes)) = ptb.inputs.first() else {
+        return Err(FanOutError::other("invalid transaction input construction"));
+    };
+    bcs::from_bytes(tx_auth_package_bytes)
+        .map_err(|_| FanOutError::other("cannot decode the bytes of input 0"))
+}
+
+#[cfg(test)]
+mod tests {
+    use walrus_test_utils::param_test;
+
+    use super::*;
+
+    struct TestPackage {
+        blob: Vec<u8>,
+        nonce: [u8; DIGEST_LEN],
+        auth_package: HashedAuthPackage,
+    }
+
+    impl TestPackage {
+        fn for_test() -> Self {
+            let blob = b"hello";
+            let nonce = [23; DIGEST_LEN];
+            Self {
+                blob: blob.to_vec(),
+                nonce,
+                auth_package: HashedAuthPackage {
+                    blob_digest: compute_digest_sha256(blob.as_slice()).digest,
+                    nonce_digest: compute_digest_sha256(&nonce).digest,
+                    unencoded_length: blob.len() as u64,
+                },
+            }
+        }
+    }
+
+    param_test! {
+        test_auth_package_checks: [
+            correct: (TestPackage::for_test(), None),
+            wrong_nonce: ({
+                let mut test_package = TestPackage::for_test();
+                test_package.nonce = [24; DIGEST_LEN];
+                test_package
+            }, Some(FanOutError::InvalidNonceHash)),
+            wrong_blob: ({
+                let mut test_package = TestPackage::for_test();
+                test_package.blob = b"world".to_vec();
+                test_package
+            }, Some(FanOutError::BlobDigestMismatch)),
+            wrong_blob_length: ({
+                let mut test_package = TestPackage::for_test();
+                test_package.auth_package.unencoded_length = 10;
+                test_package
+            }, Some(FanOutError::BlobLengthMismatch)),
+        ]
+    }
+    fn test_auth_package_checks(test_package: TestPackage, expected_error: Option<FanOutError>) {
+        let result = auth_package_checks(
+            test_package.auth_package,
+            &test_package.blob,
+            &test_package.nonce,
+        );
+
+        match (result, expected_error) {
+            (Ok(_), None) => {}
+            (Err(err), Some(expected_err)) => match (&err, &expected_err) {
+                (FanOutError::InvalidNonceHash, FanOutError::InvalidNonceHash) => {}
+                (FanOutError::BlobDigestMismatch, FanOutError::BlobDigestMismatch) => {}
+                (FanOutError::BlobLengthMismatch, FanOutError::BlobLengthMismatch) => {}
+                _ => panic!("expected error {:?} but got {:?}", expected_err, err),
+            },
+            (Err(_), None) => panic!("expected an error but got Ok"),
+            (Ok(_), Some(_)) => panic!("expected an error but got Ok"),
+        }
+    }
 }
