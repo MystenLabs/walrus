e0649d925ef85240e98623d962ceb42a3cdb2eff
giac-mysten <giacomo@mystenlabs.com>
2025-07-25 14:25:04 +0100

    fix: CLI args
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>

--- crates/walrus-service/src/client/cli/args.rs
+++ crates/walrus-service/src/client/cli/args.rs
@@ -610,23 +610,25 @@ pub enum CliCommands {
         /// The directory to pull into.
         #[arg(long)]
         backfill_dir: String,
-        /// Durable list of objects already pulled. This is loaded at script initialization and used
-        /// to avoid pulling redundant objects. Note that looking in the backfill_dir is not a
-        /// complete solution because the disk space needs to be reclaimed after the archival blobs
-        /// have been pushed up to the network. So, this `pulled_state` file is ensuring we don't
-        /// have to start from the beginning in the event that we need to stop and restart the
-        /// backfill.
+        /// Durable list of objects already pulled.
+        ///
+        /// This is loaded at script initialization and used to avoid pulling redundant objects.
+        /// Note that looking in the backfill_dir is not a complete solution because the disk space
+        /// needs to be reclaimed after the archival blobs have been pushed up to the network. So,
+        /// this `pulled_state` file is ensuring we don't have to start from the beginning in the
+        /// event that we need to stop and restart the backfill.
         #[arg(long)]
         pulled_state: PathBuf,
     },
     /// Upload blob slivers and metadata from a specified directory to the listed storage nodes.
     BlobBackfill {
-        /// The subdirectory when blob-backfill can find blobs. Blobs in this directory must be
-        /// named with their blob id. Any files that exist in this directory that do not have a
-        /// conforming blob id name will be skipped.
+        /// The subdirectory when blob-backfill can find blobs.
+        ///
+        /// Blobs in this directory must be named with their blob id. Any files that exist in this
+        /// directory that do not have a conforming blob id name will be skipped.
         #[arg(long)]
         backfill_dir: PathBuf,
-        /// The filename where successfully pushed blob IDs will be stored.
+        /// The file where successfully pushed blob IDs will be stored.
         #[arg(long)]
         pushed_state: PathBuf,
         /// The nodes to backfill with slivers and blob metadata.
