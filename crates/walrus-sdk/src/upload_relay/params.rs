b37047ff2adbfab3fe38da69bf8609b29c884e33
giac-mysten <124184614+giac-mysten@users.noreply.github.com>
2025-07-29 16:26:24 +0100

    feat: add upload relay to walrus to `walrus store` and `walrus store-quilt` (cherry-pick) (#2384)
    * feat: add upload relay to walrus to `walrus store` and `walrus store-quilt` (#2300)
    
    * feat: add walrus store --upload-relay functionality
    
    * fix: remove duplicate shared code
    
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>
    
    * feat(relay): implement tip client
    
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>
    
    * wip: add StoreArgs
    
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>
    
    * wip: add defaults to storeargs
    
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>
    
    * wip: update calls to quilt with storeargs
    
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>
    
    * wip: refactor of args complete
    
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>
    
    * wip: add retries to uploadrelayclient
    
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>
    
    * feat: client feature complete
    
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>
    
    * fix: minor log changes
    
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>
    
    * fix: improve metrics observation
    
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>
    
    * feat: add upload relay for quilts
    
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>
    
    * feat: add to quilts
    
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>
    
    * fix: rename error
    
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>
    
    * fix: rename send fn on relay
    
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>
    
    * feat: add user confirmation
    
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>
    
    * fix: add todo
    
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>
    
    * fix: comment and simtests
    
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>
    
    * fix: remove unused code from sui api
    
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>
    
    * fix: he's comments
    
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>
    
    ---------
    
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>
    Co-authored-by: giac-mysten <giacomo@mystenlabs.com>
    
    * fix: revert wrong changes from #2300 over #2122 (#2385)
    
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>
    
    * chore: bump cli version
    
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>
    
    ---------
    
    Signed-off-by: giac-mysten <giacomo@mystenlabs.com>
    Co-authored-by: Will Bradley <will.bradley@mystenlabs.com>

--- crates/walrus-sdk/src/upload_relay/params.rs
+++ crates/walrus-sdk/src/upload_relay/params.rs
@@ -1,20 +1,25 @@
 // Copyright (c) Walrus Foundation
 // SPDX-License-Identifier: Apache-2.0
 
-use anyhow::Result;
+//! The parameters and types used by the Walrus Upload Relay.
+
+use fastcrypto::hash::{HashFunction, Sha256};
+use rand::{Rng, thread_rng};
 use serde::{Deserialize, Serialize};
 use serde_with::{DisplayFromStr, serde_as};
 use sui_types::digests::TransactionDigest;
 use utoipa::IntoParams;
-use walrus_sdk::{
+
+use crate::{
     ObjectID,
     core::{BlobId, EncodingType},
     sui::ObjectIdSchema,
 };
 
-use crate::error::WalrusUploadRelayError;
-
-pub(crate) const DIGEST_LEN: usize = 32;
+/// The length of SHA256 digests in bytes.
+pub const DIGEST_LEN: usize = 32;
+/// The length of nonces in bytes.
+pub const NONCE_LEN: usize = 32;
 
 /// Schema for the `TransactionDigest` type.
 #[derive(Debug, utoipa::ToSchema)]
@@ -25,7 +30,7 @@ pub(crate) const DIGEST_LEN: usize = 32;
     description = "Sui transaction digest (or transaction ID) as a Base58 string",
     examples("EmcFpdozbobqH61w76T4UehhC4UGaAv32uZpv6c4CNyg"),
 )]
-pub(crate) struct TransactionDigestSchema(());
+pub struct TransactionDigestSchema(());
 
 /// Schema for 32-byte digests.
 #[derive(Debug, utoipa::ToSchema)]
@@ -37,33 +42,33 @@ pub(crate) struct TransactionDigestSchema(());
     description = "A 32-byte digest (or hash) as a URL-encoded (without padding) Base64 string",
     examples("rw8xIuqxwMpdOcF_3jOprsD9TtPWfXK97tT_lWr1teQ"),
 )]
-pub(crate) struct DigestSchema(());
+pub struct DigestSchema(());
 
 /// The query parameters for the Walrus Upload Relay.
 #[serde_as]
 #[derive(Debug, Deserialize, Serialize, IntoParams)]
 #[into_params(parameter_in = Query, style = Form)]
 #[serde(deny_unknown_fields)]
-pub(crate) struct Params {
+pub struct Params {
     /// The blob ID of the blob to be sent to the storage nodes.
     #[serde_as(as = "DisplayFromStr")]
     pub blob_id: BlobId,
-    /// The Base58 transaction ID of the transaction that sends the tip to the upload relay.
+    /// The Base58 transaction ID of the transaction that sends the tip to the proxy.
     ///
-    /// This field is _required_ in the case where the upload relay requires a tip.
+    /// This field is _required_ in the case where the proxy requires a tip.
     #[serde(default, skip_serializing_if = "Option::is_none")]
     #[param(value_type = Option<TransactionDigestSchema>)]
     pub tx_id: Option<TransactionDigest>,
     /// The nonce, the preimage of the hash added to the transaction inputs.
     ///
-    /// This field is _required_ in the case where the upload relay requires a tip.
+    /// This field is _required_ in the case where the proxy requires a tip.
     #[serde(
         default,
         with = "b64_option_digest",
         skip_serializing_if = "Option::is_none"
     )]
     #[param(value_type = Option<DigestSchema>)]
-    pub nonce: Option<[u8; DIGEST_LEN]>,
+    pub nonce: Option<[u8; NONCE_LEN]>,
     /// The object ID of the deletable blob to be stored.
     ///
     /// If the blob is to be stored as a permanent one, this parameter should not be specified.
@@ -78,52 +83,48 @@ pub(crate) struct Params {
     pub encoding_type: Option<EncodingType>,
 }
 
-impl Params {
-    /// Checks that the `Params` contain the `tx_id` and the `nonce`, and returns an instance of
-    /// `PaidParams`.
-    pub(crate) fn to_paid_params(&self) -> Result<PaidTipParams, WalrusUploadRelayError> {
-        let Params {
-            tx_id,
-            nonce,
-            encoding_type,
-            ..
-        } = *self;
-
-        Ok(PaidTipParams {
-            tx_id: tx_id.ok_or(WalrusUploadRelayError::MissingTxIdOrNonce)?,
-            nonce: nonce.ok_or(WalrusUploadRelayError::MissingTxIdOrNonce)?,
-            encoding_type,
-        })
-    }
-}
-
-/// The subset of query parameters of the Walrus Upload Relay, necessary to check the tip.
-///
-/// Compared to `Params`, the `tx_id` and the `nonce` are not optional, and `blob_id` and
-/// `deletable_blob_object` are not necessary.
-#[derive(Debug, Clone)]
-pub(crate) struct PaidTipParams {
-    pub tx_id: TransactionDigest,
-    pub nonce: [u8; DIGEST_LEN],
-    pub encoding_type: Option<EncodingType>,
-}
-
 /// The tip authentication structure.
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
-pub(crate) struct AuthPackage {
+pub struct AuthPackage {
     /// The SHA256 hash of the blob data.
     pub blob_digest: [u8; DIGEST_LEN],
     /// A nonce.
-    pub nonce: [u8; DIGEST_LEN],
+    pub nonce: [u8; NONCE_LEN],
     /// The length in bytes of the unencoded blob.
     pub unencoded_length: u64,
+}
+
+impl AuthPackage {
+    pub(crate) fn new(blob: &[u8]) -> Self {
+        let blob_digest = Sha256::digest(blob).digest;
+        let nonce = thread_rng().r#gen();
+        Self {
+            blob_digest,
+            nonce,
+            unencoded_length: blob.len().try_into().expect("using a u32 or u64 arch"),
+        }
+    }
+
+    pub(crate) fn to_hashed_nonce(&self) -> HashedAuthPackage {
+        let Self {
+            blob_digest,
+            nonce,
+            unencoded_length,
+        } = self.clone();
+        let nonce_digest = Sha256::digest(nonce).digest;
+        HashedAuthPackage {
+            blob_digest,
+            nonce_digest,
+            unencoded_length,
+        }
+    }
 }
 
 /// The tip authentication structure, with hashed nonce.
 ///
 /// Helper struct to serialize/deserialize with the hashed nonce.
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
-pub(crate) struct HashedAuthPackage {
+pub struct HashedAuthPackage {
     /// The SHA256 hash of the blob data.
     pub blob_digest: [u8; DIGEST_LEN],
     /// The digest of a nonce.
@@ -165,48 +166,5 @@ pub(crate) mod b64_option_digest {
         } else {
             Ok(None)
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-
-    use std::str::FromStr;
-
-    use axum::{extract::Query, http::Uri};
-    use reqwest::Url;
-    use sui_types::digests::TransactionDigest;
-    use walrus_sdk::{ObjectID, core::BlobId};
-
-    use crate::{
-        params::{DIGEST_LEN, Params},
-        shared::blob_upload_relay_url,
-    };
-
-    #[test]
-    fn test_upload_relay_parse_query() {
-        let blob_id =
-            BlobId::from_str("efshm0WcBczCA_GVtB0itHbbSXLT5VMeQDl0A1b2_0Y").expect("valid blob id");
-        let tx_id = TransactionDigest::new([13; DIGEST_LEN]);
-        let nonce = [23; DIGEST_LEN];
-        let params = Params {
-            blob_id,
-            nonce: Some(nonce),
-            deletable_blob_object: Some(ObjectID::from_single_byte(42)),
-            tx_id: Some(tx_id),
-            encoding_type: None,
-        };
-
-        let url =
-            blob_upload_relay_url(&Url::parse("http://localhost").expect("valid url"), &params)
-                .expect("valid parameters");
-
-        let uri = Uri::from_str(url.as_ref()).expect("valid conversion");
-        let result = Query::<Params>::try_from_uri(&uri).expect("parsing the uri works");
-
-        assert_eq!(params.blob_id, result.blob_id);
-        assert_eq!(params.nonce, result.nonce);
-        assert_eq!(params.tx_id, result.tx_id);
-        assert_eq!(params.deletable_blob_object, result.deletable_blob_object);
     }
 }
