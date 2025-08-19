5ad928f89c6684dc340fb455116f888a6b64a16f
Zhe Wu <zhe.wu@mystenlabs.com>
2025-08-19 00:24:17 -0700

    create per db shared block cache config

--- crates/walrus-service/src/event/event_processor/db.rs
+++ crates/walrus-service/src/event/event_processor/db.rs
@@ -72,7 +72,10 @@ impl EventProcessorStores {
         metric_conf: MetricConf,
     ) -> Result<Arc<RocksDB>> {
         let mut db_opts = rocksdb::Options::from(&db_config.global());
-        let db_table_opts_factory = DatabaseTableOptionsFactory::new(db_config.clone());
+        let db_table_opts_factory = DatabaseTableOptionsFactory::new(
+            db_config.clone(),
+            db_config.event_processor_shared_block_cache_config.clone(),
+        );
         db_opts.create_missing_column_families(true);
         db_opts.create_if_missing(true);
         let db = rocks::open_cf_opts(
