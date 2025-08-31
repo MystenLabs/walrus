e54c0a0a102af71b8a09d97fa68bfdaeb8474b60
Jean-Pierre Smith <jean-pierre@mystenlabs.com>
2025-08-31 22:05:21 +0200

    feat: tag tests with the payload size and blob count

--- tests/k6/tests/publisher/publisher_v1_put_blobs.js
+++ tests/k6/tests/publisher/publisher_v1_put_blobs.js
@@ -11,11 +11,11 @@ import { PUBLISHER_URL, PAYLOAD_SOURCE_FILE } from "../../config/environment.js"
 import { open } from 'k6/experimental/fs';
 
 /**
- * Duration over which files of each size is repeatedly stored.
+ * Number of blobs to store
  * @type {number}
- * @default 180
+ * @default 20
  */
-const STORE_DURATION_SECONDS = parseInt(__ENV.STORE_DURATION_SECONDS || '180');
+const BLOBS_TO_STORE = parseInt(__ENV.BLOBS_TO_STORE || '20');
 
 /**
  * The number of virtual users that will repeatedly store blobs.
@@ -38,17 +38,23 @@ const PAYLOAD_SIZE_CONVERSION = {
     '1Mi': 1024 * 1024,
     '10Mi': 10 * 1024 * 1024,
     '100Mi': 100 * 1024 * 1024,
+    '500Mi': 500 * 1024 * 1024,
+    '1Gi': 1024 * 1024 * 1024,
 }
 
 
 export const options = {
     scenarios: {
         "putBlobs": {
-            executor: 'constant-vus',
+            executor: 'shared-iterations',
             vus: VUS,
-            duration: `${STORE_DURATION_SECONDS}s`,
-            gracefulStop: '30s',
+            iterations: BLOBS_TO_STORE,
         }
+    },
+
+    tags: {
+        "blobs": BLOBS_TO_STORE,
+        "payload-size": PAYLOAD_SIZE,
     },
 
     // Skip TLS verification for self-signed certs.
@@ -60,10 +66,12 @@ const dataFile = await open(PAYLOAD_SOURCE_FILE);
 export function setup() {
     console.log('');
     console.log(`Publisher URL: ${PUBLISHER_URL}`);
-    console.log(`Store duration: ${STORE_DURATION_SECONDS}s`);
+    console.log(`Blobs to store: ${BLOBS_TO_STORE}`);
     console.log(`Virtual users: ${VUS}`);
     console.log(`Data file path: ${PAYLOAD_SOURCE_FILE}`);
     console.log(`Payload size: ${PAYLOAD_SIZE}`);
+
+
 }
 
 export default async function () {
