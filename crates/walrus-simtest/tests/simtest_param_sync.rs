8d97b57d1c4cf30a79206763fbc42f4e262c42c4
Will Bradley <will.bradley@mystenlabs.com>
2025-09-18 14:15:39 -0600

    updates

--- crates/walrus-simtest/tests/simtest_param_sync.rs
+++ crates/walrus-simtest/tests/simtest_param_sync.rs
@@ -236,7 +236,7 @@ mod tests {
         assert!(walrus_cluster.nodes[5].node_id.is_none());
         let client_arc = Arc::new(client);
 
-        let new_address = UnusedSocketAddressIp::DistinctIp
+        let new_address = UnusedSocketAddress::DistinctIp
             .try_into()
             .expect("should get unused socket address");
         let network_key_pair = walrus_core::keys::NetworkKeyPair::generate();
