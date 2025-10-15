d1b764a1af0a5b1d70a9d847db664c204f655436
Zhe Wu <zhe.wu@mystenlabs.com>
2025-10-14 21:04:08 -0700

    basic load shedding for aggregators

--- crates/walrus-service/src/client/cli/runner.rs
+++ crates/walrus-service/src/client/cli/runner.rs
@@ -1245,8 +1245,7 @@ impl ClientCommandRunner {
             client,
             daemon_args.bind_address,
             registry,
-            aggregator_args.allowed_headers,
-            aggregator_args.allow_quilt_patch_tags_in_response,
+            &aggregator_args,
         ))
     }
 
