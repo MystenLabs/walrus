e3f3b4c85725445c7978b26214634ce7efc60953
jessiemongeon1 <jessie.mongeon@mystenlabs.com>
2025-12-11 09:50:46 -0600

    fix walrus pdf

---
+++ docs/site/src/pages/walrus-pdf.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function WalrusPDF() {
+  return (
+    <Layout title="Walrus PDF">
+      <div style={{ width: '100%', height: '100vh' }}>
+        <iframe
+          src="/walrus.pdf"
+          style={{ width: '100%', height: '100%', border: 'none' }}
+          title="Walrus PDF"
+        />
+      </div>
+    </Layout>
+  );
+}
