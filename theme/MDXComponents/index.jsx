d34ff8d4016f7ee7c6dc35658f4c302be26709a4
jessiemongeon1 <jessie.mongeon@mystenlabs.com>
2026-01-07 13:11:27 -0600

    Squashed 'docs/site/src/shared/' content from commit a12e42f4
    git-subtree-dir: docs/site/src/shared
    git-subtree-split: a12e42f47abe5ef999c396e381cb7662e925c01b

---
+++ theme/MDXComponents/index.jsx
@@ -0,0 +1,9 @@
+//
+// This code ensures <Tabs> <TabItems> are in the MDX scope globally
+
+import React from "react";
+import MDXComponents from "@theme-original/MDXComponents";
+
+export default {
+  ...MDXComponents,
+};
